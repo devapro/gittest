31f0f79c020cccc0250be5a045d0bd31d872dd50
Arsenii Kharlanow <arsenyzp@gmail.com>
2022-08-18 17:21:56 +0300

    commit 1

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,30 @@
 #! /bin/bash
 
-RELEASE_NAME = "0.0.1"
+#### Create new release branch
 
-git checkout -b v$RELEASE_NAME
+RELEASE_NAME="0.0.3"
+
+git checkout develop
+
+git pull
+
+branches=( $(git branch -a) )
+
+RELEASE_BRANCH_NAME="v${RELEASE_NAME}"
+
+RELEASE_BRANCH_CREATED=0
+
+[[ " ${branches[@]} " =~ " ${RELEASE_BRANCH_NAME} " ]] && RELEASE_BRANCH_CREATED=1 || RELEASE_BRANCH_CREATED=0;
+
+if [ $RELEASE_BRANCH_CREATED -eq 0 ]; then
+	git tag "release_${RELEASE_BRANCH_NAME}"
+	git checkout -b ${RELEASE_BRANCH_NAME}
+else
+	echo "${RELEASE_BRANCH_NAME} exist"
+fi
+
+#### Create change log
+
+git log --oneline --decorate
+git log --pretty=”%s”
+git log --pretty="- %s"
